d4f6b1bbe71330a02b324fd44f1e59b6c738565d
Robert Rees <shudderfix@gmail.com>
2023-02-23 15:40:26 +0000

    Adds a parsing regular expression to the code

--- internal/dice/dice_string.go
+++ internal/dice/dice_string.go
@@ -1,11 +1,13 @@
 package dice
 
 import (
+	"fmt"
 	"regexp"
+	"strconv"
 )
 
 var NoModiferRegExp = regexp.MustCompile(`^\d+d(\d)+$`)
-var DiceStringRegExp = regexp.MustCompile(`^(\d+)d(\d+)[+-](\d+)$`)
+var DiceStringRegExp = regexp.MustCompile(`^(?P<Size>\d+)d(?P<Sides>\d+)(?P<Modifier>[+-]\d+)$`)
 
 func normaliseString(diceString string) string {
 	if NoModiferRegExp.MatchString(diceString) {
@@ -13,4 +15,34 @@ func normaliseString(diceString string) string {
 	}
 
 	return diceString
+}
+
+func parseString(diceString string) RollRequest {
+	normaliseDiceString := normaliseString(diceString)
+	match := DiceStringRegExp.FindStringSubmatch(normaliseDiceString)
+
+	sizeString := match[DiceStringRegExp.SubexpIndex("Size")]
+
+	poolSize, err := strconv.Atoi(sizeString)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't parse %s, %s was not an integer", diceString, sizeString))
+	}
+
+	sidesString := match[DiceStringRegExp.SubexpIndex("Sides")]
+	sides, err := strconv.Atoi(sidesString)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't parse %s, %s was not an integer", diceString, sidesString))
+	}
+
+	modifierString := match[DiceStringRegExp.SubexpIndex("Modifier")]
+	modifier, err := strconv.Atoi(modifierString)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't parse %s, %s was not an integer", diceString, modifierString))
+	}
+
+	return RollRequest{
+		poolSize,
+		sides,
+		modifier,
+	}
 }
--- internal/dice/dice_string_test.go
+++ internal/dice/dice_string_test.go
@@ -1,6 +1,7 @@
 package dice
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -20,4 +21,32 @@ func Test_diceStringNormalisationForProvidedModifiers(t *testing.T) {
 	if result != "1d6+2" {
 		t.Error("Dice string ", inputString, " not normalised, got ", result)
 	}
+}
+
+func Test_diceStringCreatesRollRequest(t *testing.T) {
+	for _, testData := range []struct{
+		Input string
+		ExpectedPool int
+		ExpectedSides int
+		ExpectedModifier int
+	}{
+		{"11d20-4", 11, 20, -4,},
+		{"1d6", 1, 6, 0},
+		{"2d4+1", 2, 4, 1},
+	} {
+		result := parseString(testData.Input)
+
+		if result.NumberOfDice != testData.ExpectedPool {
+			t.Error(fmt.Sprintf("Wrong number of dice read: expected %d and got %d ", testData.ExpectedPool, result.NumberOfDice))
+		}
+
+		if result.Sides != testData.ExpectedSides {
+			t.Error(fmt.Sprintf("Wrong number of sides requested, expected %d and got %d", testData.ExpectedSides, result.Sides))
+		}
+
+		if result.Modifier != testData.ExpectedModifier {
+			t.Error(fmt.Sprintf("Wrong modifer requested, expected %d and got %d", testData.ExpectedModifier, result.Modifier))
+		}
+	}
+
 }
